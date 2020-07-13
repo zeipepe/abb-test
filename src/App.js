de5eba49ea36cf5eafb8a6aa35d3be7c2e6d3a3b
Ezequiel Pepe <ezequiel.ignacio.pepe@gmail.com>
2020-07-13 14:16:35 +0200

    feat: create view and add more mock data

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Part from "./components/Part/Part";
+import Parts from "./views/Parts/Parts";
 function App() {
-  const part = {
-    name: "Part A",
-    features: [
-      {
-        name: "Feature Name",
-        controls: [{ name: "control", dev: 0, devTol: 0, status: "ok" },{ name: "control", dev: 0, devTol: 0, status: "warning" },{ name: "control", dev: 0, devTol: 0, status: "error" },{ name: "control", dev: 0, devTol: 0, status: "ok" },{ name: "control", dev: 0, devTol: 0, status: "ok" }],
-      },
-    ],
-  };
 
   return (
     <div className="App">
-      <Part name={part.name} features={part.features} />
+      <Parts />
     </div>
   );
 }
---
+++ src/views/Parts/Parts.js
@@ -0,0 +1,64 @@
+import React from "react";
+import Part from "../../components/Part/Part";
+function Parts() {
+  const parts = [
+    {
+      name: "Part A",
+      features: [
+        {
+          name: "Feature Name",
+          controls: [
+            { name: "control", dev: 0, devTol: 0, status: "ok" },
+            { name: "control", dev: 0, devTol: 0, status: "warning" },
+            { name: "control", dev: 0, devTol: 0, status: "error" },
+            { name: "control", dev: 0, devTol: 0, status: "ok" },
+            { name: "control", dev: 0, devTol: 0, status: "ok" },
+            { name: "control", dev: 0, devTol: 0, status: "ok" },
+            { name: "control", dev: 0, devTol: 0, status: "warning" },
+            { name: "control", dev: 0, devTol: 0, status: "error" },
+            { name: "control", dev: 0, devTol: 0, status: "ok" },
+            { name: "control", dev: 0, devTol: 0, status: "ok" },
+            { name: "control", dev: 0, devTol: 0, status: "ok" },
+            { name: "control", dev: 0, devTol: 0, status: "warning" },
+            { name: "control", dev: 0, devTol: 0, status: "error" },
+            { name: "control", dev: 0, devTol: 0, status: "ok" },
+            { name: "control", dev: 0, devTol: 0, status: "ok" },
+          ],
+        },
+        {
+          name: "Feature's Name",
+          controls: [
+            { name: "control", dev: 0, devTol: 0, status: "ok" },
+            { name: "control", dev: 0, devTol: 0, status: "warning" },
+            { name: "control", dev: 0, devTol: 0, status: "ok" },
+            { name: "control", dev: 0, devTol: 0, status: "ok" },
+          ],
+        },
+      ],
+    },
+    {
+      name: "Part A",
+      features: [
+        {
+          name: "Feature Name",
+          controls: [
+            { name: "control", dev: 0, devTol: 0, status: "ok" },
+            { name: "control", dev: 0, devTol: 0, status: "warning" },
+            { name: "control", dev: 0, devTol: 0, status: "ok" },
+            { name: "control", dev: 0, devTol: 0, status: "ok" },
+          ],
+        },
+      ],
+    },
+  ];
+
+  return (
+    <div className="App">
+      {parts.map((part, index) => (
+        <Part key={"part-" + index} name={part.name} features={part.features} />
+      ))}
+    </div>
+  );
+}
+
+export default Parts;
